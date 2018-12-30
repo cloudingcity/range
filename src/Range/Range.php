cd7be3857a36cd406651923915cd90dd54df35e5
Clouding <ghost0436@gmail.com>
2018-12-30 17:23:08 +0800

    Implement equals feature

--- src/Range/Range.php
+++ src/Range/Range.php
@@ -84,8 +84,30 @@ class Range
      * @param  int  $integer
      * @return bool
      */
-    public function isNotContains(int $integer)
+    public function isNotContains(int $integer): bool
     {
         return ! $this->contains($integer);
+    }
+
+    /**
+     * Determine if the range is equals given range or not.
+     *
+     * @param  \Clouding\Range\Range  $range
+     * @return bool
+     */
+    public function equals(Range $range): bool
+    {
+        return $this->start === $range->start() && $this->end === $range->end();
+    }
+
+    /**
+     * Determine if the range is not equals given range.
+     *
+     * @param  \Clouding\Range\Range  $range
+     * @return bool
+     */
+    public function isNotEquals(Range $range): bool
+    {
+        return ! $this->equals($range);
     }
 }
--- tests/Range/RangeTest.php
+++ tests/Range/RangeTest.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace Clouding\Range\Tests;
 
 use Clouding\Range\Range;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
 
 class RangeTest extends TestCase
 {
+    use MockeryPHPUnitIntegration;
+
     /**
      * @var \Clouding\Range\Range
      */
@@ -52,17 +56,25 @@ class RangeTest extends TestCase
         $this->assertTrue($range->contains(1));
         $this->assertTrue($range->contains(10));
         $this->assertTrue($range->contains(5));
+
         $this->assertTrue($range->isNotContains(0));
         $this->assertTrue($range->isNotContains(11));
         $this->assertTrue($range->isNotContains(-8));
     }
 
-    public function testIsNotContains()
+    public function testEquals()
     {
-         $range = new Range(1, 10);
+        $range = new Range(1, 10);
 
-        $this->assertTrue($range->isNotContains(0));
-        $this->assertTrue($range->isNotContains(11));
-        $this->assertTrue($range->isNotContains(-8));
+        $mockRange = Mockery::mock(Range::class);
+        $mockRange->shouldReceive('start')
+            ->twice()
+            ->andReturn(1);
+        $mockRange->shouldReceive('end')
+            ->twice()
+            ->andReturn(10);
+
+        $this->assertTrue($range->equals($mockRange));
+        $this->assertFalse($range->isNotEquals($mockRange));
     }
 }
